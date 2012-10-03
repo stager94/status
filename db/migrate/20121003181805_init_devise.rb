c365989f7d825727f181443192f7ae411ec81f99
stager94 <stager94@mail.ru>
2012-10-03 21:26:59 +0300

    message

--- db/migrate/20121003181805_init_devise.rb
+++ db/migrate/20121003181805_init_devise.rb
@@ -35,10 +35,10 @@ class InitDevise < ActiveRecord::Migration
 
 
       # Uncomment below if timestamps were not included in your original model.
-      t.timestamps
+      # t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    # add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :confirmation_token,   :unique => true
     add_index :users, :unlock_token,         :unique => true
